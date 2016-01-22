72c883f6365de015087054e7dd93a3ad94365485
Usman Bashir <me@usmanbashir.com>
2016-01-22 22:16:13 +0300

    Allow a poll to share its vote results

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -4,5 +4,35 @@ class Poll
 
   # Relations
   belongs_to :event
-  has_many :votes,        dependent: :restrict
+  has_many :votes, dependent: :restrict
+
+
+  # Default Scopes
+
+  default_scope -> { order(created_at: :desc) }
+
+
+  # JSON Fields
+  def as_json(options={})
+    super(methods: [:votes_total, :away_team, :draw, :home_team])
+  end
+
+
+  # Methods
+  
+  def votes_total
+    votes.count
+  end
+
+  def away_team
+    votes.where(answer: "AWAY_TEAM").count
+  end
+
+  def draw
+    votes.where(answer: "DRAW").count
+  end
+
+  def home_team
+    votes.where(answer: "HOME_TEAM").count
+  end
 end
