a5b31e131553776eb96b65cfc67b4b8946ca484b
Usman Bashir <me@usmanbashir.com>
2016-01-22 06:08:25 +0300

    Add events model along with some seed data.

---
+++ app/models/event.rb
@@ -0,0 +1,29 @@
+class Event
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  # Fields
+  field :objectId,      type: String
+
+  field :away_name,     type: String          # Away Team Name
+  field :home_name,     type: String          # Home Team Name
+  field :group,         type: String          # Group Name
+  field :name,          type: String          # Match Name
+
+  field :sport,         type: String,
+                        default: "FOOTBALL"   # Sport Name, For future needs perhaps?
+  field :state,         type: String          # Current Match State
+
+  field :start,         type: DateTime        # Match Start Date
+
+
+  # Index
+  index({ objectId: 1 }, { unique: true })
+
+
+  # Validations
+  validates :objectId, uniqueness: true
+
+  validates :objectId, :away_name, :home_name, 
+            :group, :state, :start, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+seed_data = [
+  {
+    away_name: "Panthrakikos Komotini",
+    created_at: "2015-12-18T12:30:39.228Z",
+    group: "Greek Cup",
+    home_name: "Chania FC",
+    _id: 1002916450,
+    name: "Chania FC - Panthrakikos Komotini",
+    objectId: "1UaQjc7lIb",
+    sport: "FOOTBALL",
+    start: "2015-12-17T13:00Z",
+    state: "STARTED",
+    updated_at: "2015-12-18T12:30:39.228Z"
+  },
+  {
+    away_name: "PAOK Thessaloniki",
+    created_at: "2015-12-18T12:30:39.234Z",
+    group: "Greek Cup",
+    home_name: "Olympiakos Volos",
+    _id: 1002916451,
+    name: "Olympiakos Volos - PAOK Thessaloniki",
+    objectId: "UPJ240T2Qj",
+    sport: "FOOTBALL",
+    start: "2015-12-17T13:00Z",
+    state: "STARTED",
+    updated_at: "2015-12-18T12:30:39.234Z"
+  }
+]
+
+seed_data.each {|event| Event.create(event)}
