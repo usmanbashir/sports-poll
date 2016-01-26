1ec366d7e6c011f035f72735baec4909f6f19681
Usman Bashir <me@usmanbashir.com>
2016-01-26 06:08:56 +0300

    Add a skeleton template to serve as the container for all pages

---
+++ frontend/components/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+
+        <nav>
+          <div className="nav-wrapper container">
+            <Link to="/" className="brand-logo left">Sports Poll</Link>
+            <ul id="nav-mobile" className="right">
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="container">
+          {this.props.children}
+        </div>
+
+        <footer className="page-footer">
+          <div className="container">
+            <div className="row">
+              <div className="col s12">
+                <p className="grey-text text-lighten-4">
+                  &copy; {new Date().getFullYear()} Sports Poll Inc.
+                </p>
+              </div>
+            </div>
+          </div>
+        </footer>
+
+      </div>
+    );
+  }
+}
