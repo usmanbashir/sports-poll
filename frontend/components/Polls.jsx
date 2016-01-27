15784b68f33757f0542321a3cc1d21d8c85b95c0
Usman Bashir <me@usmanbashir.com>
2016-01-27 18:19:31 +0300

    Add real mock data to drive the Polls UI

--- frontend/components/Polls.jsx
+++ frontend/components/Polls.jsx
@@ -6,9 +6,38 @@ import PollsList from "./PollsList";
 let getState = () => {
   return {
     pollsList: [
-      { id: 1, name: "Mock 1" },
-      { id: 2, name: "Mock 2" },
-      { id: 3, name: "Mock 3" }
+      {
+        id:               "1XXXXXXXXXXXXXXXXXXXXXXX",
+        votes_total:      4,
+        away_team:        1,
+        draw:             3,
+        home_team:        0,
+        created_at:       "2016-01-22T17:26:18.985Z",
+        event: {
+          id:             1002916450,
+          home_team:      "Chania FC",
+          home_team_logo: "assets/images/events/chania_fc.jpg",
+          away_team:      "Panthrakikos Komotini",
+          away_team_logo: "assets/images/events/panthrakikos_komotini.png",
+          group:          "Greek Cup"
+        }
+      },
+      {
+        id:               "2XXXXXXXXXXXXXXXXXXXXXXX",
+        votes_total:      26,
+        away_team:        5,
+        draw:             13,
+        home_team:        9,
+        created_at:       "2016-01-22T17:26:17.985Z",
+        event: {
+          id:             1002916451,
+          home_team:      "Olympiakos Volos",
+          home_team_logo: "assets/images/events/olympiakos_volos.png",
+          away_team:      "PAOK Thessaloniki",
+          away_team_logo: "assets/images/events/paok_thessaloniki.png",
+          group:          "Greek Cup"
+        }
+      }
     ]
   }
 }
