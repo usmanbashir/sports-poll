69eaad65483ef0acf835518d73928c52a2518f46
Usman Bashir <me@usmanbashir.com>
2016-01-29 07:13:13 +0300

    Seed real events data to MongoDB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,34 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require "json"
 
-seed_data = [
-  {
-    away_name: "Panthrakikos Komotini",
-    created_at: "2015-12-18T12:30:39.228Z",
-    group: "Greek Cup",
-    home_name: "Chania FC",
-    _id: 1002916450,
-    name: "Chania FC - Panthrakikos Komotini",
-    objectId: "1UaQjc7lIb",
-    sport: "FOOTBALL",
-    start: "2015-12-17T13:00Z",
-    state: "STARTED",
-    updated_at: "2015-12-18T12:30:39.228Z"
-  },
-  {
-    away_name: "PAOK Thessaloniki",
-    created_at: "2015-12-18T12:30:39.234Z",
-    group: "Greek Cup",
-    home_name: "Olympiakos Volos",
-    _id: 1002916451,
-    name: "Olympiakos Volos - PAOK Thessaloniki",
-    objectId: "UPJ240T2Qj",
-    sport: "FOOTBALL",
-    start: "2015-12-17T13:00Z",
-    state: "STARTED",
-    updated_at: "2015-12-18T12:30:39.234Z"
-  }
-]
+seed_file = File.read("db/events_seed.json")
 
-seed_data.each {|event| Event.create(event)}
+seed_data = JSON.parse(seed_file)
+
+seed_data["results"].each do |data|
+  event = Event.create({
+    away_name:    data["awayName"],
+    created_at:   data["createdAt"],
+    group:        data["group"],
+    home_name:    data["homeName"],
+    _id:          data["id"],
+    name:         data["name"],
+    objectId:     data["objectId"],
+    sport:        data["sport"],
+    start:        data["start"],
+    state:        data["state"],
+    updated_at:   data["updatedAt"]
+  })
+
+  event.create_poll
+end
